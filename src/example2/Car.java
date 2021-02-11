bc95c5a4cf20e5e7bbbad771bbfd66f8aec8ccfb
tomasjakub <tomasjakub21@gmail.com>
2021-02-11 14:32:14 +0100

    "first commit"

---
+++ src/example2/Car.java
@@ -0,0 +1,25 @@
+package example2;
+
+public class Car {
+    private String name;
+
+    public void nameToUppercase(String name) {
+        System.out.println("your name to uppercase is " + name.toUpperCase());
+
+    }
+
+
+
+    /*public void nameToUppercase(String name){
+        System.out.println("your name to uppercase is " + name.toUpperCase());
+    }*/
+
+    /*public void nameToUppercase() {
+        System.out.println("your name to uppercase is " + name.toUpperCase());
+
+    }*/
+
+   /* public Car(String name){
+        System.out.println("your name to uppercase is " + name.toUpperCase());
+    }*/
+}
---
+++ src/example2/MyInterface.java
@@ -0,0 +1,6 @@
+package example2;
+
+@FunctionalInterface
+public interface MyInterface {
+    void display(String name);
+}
